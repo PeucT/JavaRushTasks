4af97db5ff4dfebe712195b276f7e59facdc48d4
PeucT <cool_math@mail.ru>
2017-02-09 18:32:40 +0300

    Level 20 added

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2021/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task20.task2021;
+
+import java.io.*;
+
+/* 
+Запрет сериализации
+*/
+public class Solution implements Serializable {
+    public static class SubSolution extends Solution {
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
