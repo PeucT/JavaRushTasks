2066a9d9b409a7d4af017593120c972557b906bb
PeucT <cool_math@mail.ru>
2017-05-12 16:35:08 +0400

    Level 31. 3106

--- 4.JavaCollections/src/com/javarush/task/task31/task3106/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3106/Solution.java
@@ -3,39 +3,49 @@ package com.javarush.task.task31.task3106;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
-import java.util.zip.ZipOutputStream;
 
 /*
 Разархивируем файл
 */
 public class Solution {
     public static void main(String[] args) throws IOException {
-        if (args.length < 2) {
-            return;
+        if (args == null) { return; }
+        List<String> pathsList = new ArrayList<String>();
+
+        for (int i = 1; i < args.length; i++) {
+            pathsList.add(args[i]);
         }
-        List<String> list = new ArrayList<>();
-        List list1 = new ArrayList<>();
-        for (int i = 1; i< args.length; i++) {
-            list.add(args[i]);
-        }
-        Collections.sort(list);
-        for (int i = 0; i < list.size(); i++) {
-            list1.add(new FileInputStream(list.get(i)));
-        }
-        byte[] bytes = new byte[1024];
-        String resultFileName = args[0];
-        int readByteCnt;
-        try (ZipInputStream zipInputStream = new ZipInputStream(new SequenceInputStream(Collections.enumeration(list1)));
-             ZipOutputStream outputStream = new ZipOutputStream(new FileOutputStream(resultFileName))) {
-            while (zipInputStream.available() >0) {
-                zipInputStream.getNextEntry();
-                while ((readByteCnt = zipInputStream.read(bytes)) > 0) {
-                    outputStream.write(bytes, 0, readByteCnt);
-                    outputStream.flush();
-                }
+
+        Collections.sort(pathsList, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.substring(o1.length() - 3).compareTo(o2.substring(o2.length() - 3));
             }
+        });
+
+        List<FileInputStream> streamsLists = new ArrayList<>();
+        for (int i = 0; i < pathsList.size(); i++) {
+            streamsLists.add(new FileInputStream(pathsList.get(i)));
         }
+
+        ZipInputStream fullZipInput = new ZipInputStream(new SequenceInputStream(Collections.enumeration(streamsLists)));
+        ZipEntry zipEntry = null;
+        FileOutputStream out = new FileOutputStream(new File(args[0]));
+        while ((zipEntry = fullZipInput.getNextEntry()) != null) {
+            int symbol = 0;
+            int count;
+            byte[] buff = new byte[1024 * 1024];
+            while ((count = fullZipInput.read(buff)) != -1) {
+                out.write(buff, 0, count);
+            }
+            out.flush();
+        }
+        fullZipInput.close();
+        out.close();
+
     }
 }
